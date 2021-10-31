c657fd34fd0a47a36f9593c446c42ec2a888deed
tom-rt <thomraout@gmail.com>
2021-10-31 11:51:07 +0100

    fixed scrolling and global css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -39,7 +39,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App bg-gray-500">
+      <div className="App bg-gray-50">
           <TopBar />
           <MainContainer />
       </div>
