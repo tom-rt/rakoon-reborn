54eba4b4c4b5dd417e176ed44cddcdf14dc85f11
Thomas Raout <thomas.raout@neoxia.com>
2021-12-21 18:57:00 +0100

    design for desktop begin

--- client/src/components/desktop/Desktop.tsx
+++ client/src/components/desktop/Desktop.tsx
@@ -1,23 +1,32 @@
 import { GrpcService } from "../../services/grpc.service";
-import { Context } from '../../context'
-import React, { FormEvent, useContext, useEffect } from "react"
+import { Context } from "../../context";
+import React, { FormEvent, useContext, useEffect } from "react";
 
 function Desktop() {
-
   const [state, setState] = React.useState({
     userName: "",
     password: "",
-    isFormValid: false
-  })
+    isFormValid: false,
+  });
 
-  const context = useContext(Context)
-  const grpcService = new GrpcService()
+  const context = useContext(Context);
+  const grpcService = new GrpcService();
 
-    return (
-      <div className="Desktop flex w-full">
-          Desktop {context.user.username}
+  return (
+    <div className="Desktop flex flex-col w-full">
+      <div className="flex flex-row w-full border-b-2 mb-2 pb-2 border-blue-500 items-end">
+        <button className="bg-blue-500 h-12 w-fit px-3 py-3 flex items-center rounded mr-2">
+          <div className="fredoka text-xl text-gray-100 mr-2 flex items-center">
+            +
+          </div>
+          <div className="text-base text-gray-100">Nouveau</div>
+        </button>
+        <div className="text-lg">
+          Bureau {context.user.username} {">"}
+        </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Desktop;
--- client/src/components/main-container/MainContainer.tsx
+++ client/src/components/main-container/MainContainer.tsx
@@ -1,47 +1,46 @@
-import React from 'react';
-import Login from '../login/Login';
+import React from "react";
+import Login from "../login/Login";
 import ReactDOM from "react-dom";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate
+  Navigate,
 } from "react-router-dom";
 
-
-import Desktop from '../desktop/Desktop';
+import Desktop from "../desktop/Desktop";
 
 class MainContainer extends React.Component {
   constructor(props: any) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
 
-  componentDidMount() {
-  }
+  componentDidMount() {}
 
-  componentWillUnmount() {  }
+  componentWillUnmount() {}
 
   render() {
     return (
-      <div className="MainContainer bg-gray-50 flex flex-col w-full p-24">
-  <Router>
-  <Routes>
-        <Route path="/" element={<Navigate to ="/login" />}/>
-        <Route path="/login" element={<Login />} />
-        <Route path="/desktop" element={<Desktop />} />
-        <Route path="*" element={
-          <main style={{ padding: "1rem" }}>
-            <p>There's nothing here!</p>
-          </main>
-        }
-        />
-  </Routes>
+      <div className="MainContainer bg-gray-50 flex flex-col w-full pt-24 px-8">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/desktop" element={<Desktop />} />
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>There's nothing here!</p>
+                </main>
+              }
+            />
+          </Routes>
         </Router>
       </div>
     );
   }
-
 }
 
 export default MainContainer;
